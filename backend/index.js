546a0dcc4dcd2adacc187ea3d279d372c3f19971
ItsJoey <ItsJoeySan@gmail.com>
2023-11-30 16:31:37 +0530

    Intial setup for the project frontend using React

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,25 @@ import express, { response } from 'express';
 import { PORT, URI } from './config.js';
 import mongoose from 'mongoose';
 import booksRoute from './routes/booksRoute.js';
-
+import cors from "cors";    
 
 const app = express();
 app.use(express.json());
 app.use("/books", booksRoute);
+
+//cors policy error is a kind of error that arrives when we try to 
+// request data from another domain so if you want to access different domains use cors
+
+//Option 1: allow everything NOTE: this could be harmful
+// app.use(cors());
+
+//Option 2: allow custom origins
+app.use(cors({
+    origin: "http:localhost:5173",
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}));
+
 
 mongoose.connect(URI)
 .then(()=>{
