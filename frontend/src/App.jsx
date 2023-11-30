3e409e4b43a5b981964ae8c7dd56eefe662d7488
ItsJoey <ItsJoeySan@gmail.com>
2023-11-30 16:53:55 +0530

    Created 5 pages & Linked using react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import EditBook from './pages/EditBook.jsx';
+import ShowBook from './pages/ShowBook.jsx';
+import InsertBook from './pages/InsertBook.jsx';
+import DeleteBook from './pages/DeleteBook.jsx';
 
 function App() {
 
   return (
     <>
       <h1>Hello</h1>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/edit-book' element={<EditBook/>} />
+        <Route path='/show-book' element={<ShowBook/>} />
+        <Route path='/upload-book' element={<InsertBook />} />
+        <Route path='/delete-book' element={<DeleteBook />} />
+      </Routes>
     </>
   )
 }
---
+++ frontend/src/pages/InsertBook.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function InsertBook() {
+  return (
+    <div>InsertBook</div>
+  )
+}
